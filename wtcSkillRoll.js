55115d05c62294d6c3135bd5825808802e08c23e
Kristen May <kristen.may@ivanti.com>
2021-04-07 18:59:48 -0600

    WIP - only show attribute bonus row if something other than attribute is selected

--- wtcSkillRoll.js
+++ wtcSkillRoll.js
@@ -1,4 +1,8 @@
-let initialSkill = 'Fight[S]';
+/** Simple dialog to roll Fudge dice with 3 categories of bonuses for Wearing the Cape
+  * Adapted from the Animated Objects attack/damage macro by HoneyBadger
+*/
+
+let initialSkill = 'Four[A]';
 let skillsOptions = getSkillsOptions();
 let initialSkillBonus = skillsOptions?.find(s => s.name == initialSkill).value;
 
@@ -6,26 +10,28 @@ function getSkillsOptions() {
   if (actor === undefined) {
     return null;
   }
-  return actor.data.items
-    .filter(item => item.type === 'skill' && item.name != '[ATTRIBUTES]' && item.name != '[SKILLS]' && item.name != '[RESOURCES]')
-    .map(function (element) {
-      return { name: element.name, value: element.data.rank }
-    })
-    .sort((a, b) => (a.name > b.name) ? 1 : -1);
+  return [
+    {name: "One[A]", value: "1"},
+    {name: "Two[S]", value: "2"},
+    {name: "Three[R]", value: "3"},
+    {name: "Four[A]", value: "4"},
+    {name: "Five[S]", value: "5"},
+    {name: "Six[R]", value: "6"},
+  ]
 }
 
 function roll() {
   var skill = document.getElementById("skillDropDown");
+  var attributeBonus = document.getElementById("attributeBonus").value;
   var aspectBonus = document.getElementById("aspectBonus").value;
-  var adjustments = document.getElementById("adjustments").value;
-  let roll = new Roll(`4df+${skill.value ? skill.value : 0}+${aspectBonus ? aspectBonus : 0}+${adjustments ? adjustments : 0}`).roll();
-  roll.toMessage({ flavor: `4df ${skill.options[skill.selectedIndex].text} Skill Roll - click to see the dice` });
+  var circumstanceModifier = document.getElementById("circumstanceModifier").value;
+  console.log(`4df + skill:${skill.value ? skill.value : 0} + attributeBonus:${attributeBonus ? attributeBonus : 0} + aspectBonus:${aspectBonus ? aspectBonus : 0} + circumstanceModifier:${circumstanceModifier ? circumstanceModifier : 0}`);
 }
 
 let dialogContent = `
   <script>
     function SkillChanged(selectedSkill) {
-      document.getElementById("skillBonus").innerHTML = (selectedSkill.value >= 0? '+': '') + selectedSkill.value
+      document.getElementById("skillBonus").innerHTML = (selectedSkill.value >= 0? '+': '') + selectedSkill.value;
     }
   </script>
 
@@ -42,9 +48,17 @@ let dialogContent = `
       </td>
       <td style="width:33%;border 1px solid black;padding:5px" id="skillBonus">${(initialSkillBonus >= 0 ? '+' : '') + initialSkillBonus}</td>
     </tr>
+    <tr id="attributeBonusRow" style="display='none'">
+      <th style="width:33%">
+        <label id="attributeBonusLabel">Attribute Bonus</label>
+      </th> 
+      <td style="width:33%">
+      <input type = "text" id="attributeBonus" \>
+      </td>
+    </tr>
     <tr>
       <th style="width:33%">
-        <label>Aspect Bonuses</label>
+        <label>Aspect Bonus</label>
       </th> 
       <td style="width:33%">
       <input type = "text" id="aspectBonus" \>
@@ -52,10 +66,10 @@ let dialogContent = `
     </tr>
     <tr>
       <th style="width:33%">
-        <label>Adjustments</label>
+        <label>Circumstance Modifier</label>
       </th> 
       <td style="width:33%">
-        <input type = "text" id="adjustments" \>
+        <input type = "text" id="circumstanceModifier" \>
       </td>
     </tr>
   </table>
