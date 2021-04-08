3bfc6d56957d8a71d12b3b1458081a19b8db9839
Kristen May <kristen.may@ivanti.com>
2021-04-07 20:37:07 -0600

    Group select options by attributes, skills, and resources.

--- wtcSkillRoll.js
+++ wtcSkillRoll.js
@@ -6,18 +6,29 @@ let initialSkill = 'Four[A]';
 let skillsOptions = getSkillsOptions();
 let initialSkillBonus = skillsOptions?.find(s => s.name == initialSkill).value;
 
+let attributes = skillsOptions?.filter(item => item.name.includes('[A]')).sort((a, b) => (a.name > b.name) ? 1 : -1);
+let skills = skillsOptions?.filter(item => item.name.includes('[S]')).sort((a, b) => (a.name > b.name) ? 1 : -1);
+let resources = skillsOptions?.filter(item => item.name.includes('[R]')).sort((a, b) => (a.name > b.name) ? 1 : -1);
+
 function getSkillsOptions() {
   if (actor === undefined) {
     return null;
   }
-  return [
-    {name: "One[A]", value: "1"},
-    {name: "Two[S]", value: "2"},
-    {name: "Three[R]", value: "3"},
-    {name: "Four[A]", value: "4"},
-    {name: "Five[S]", value: "5"},
-    {name: "Six[R]", value: "6"},
-  ];
+  return actor.data.items
+    .filter(item => item.type === 'skill' && item.name != '[ATTRIBUTES]' && item.name != '[SKILLS]' && item.name != '[RESOURCES]')
+    .map(function (element) {
+      return { name: element.name, value: element.data.rank }
+    });
+    
+  // For Testing
+  // return [
+  //   { name: "One[A]", value: "1" },
+  //   { name: "Two[S]", value: "2" },
+  //   { name: "Three[R]", value: "3" },
+  //   { name: "Four[A]", value: "4" },
+  //   { name: "Five[S]", value: "5" },
+  //   { name: "Six[R]", value: "6" },
+  // ];
 }
 
 function roll() {
@@ -28,10 +39,10 @@ function roll() {
   console.log(`4df + skill:${skill.value ? skill.value : 0} + attributeBonus:${attributeBonus ? attributeBonus : 0} + aspectBonus:${aspectBonus ? aspectBonus : 0} + circumstanceModifier:${circumstanceModifier ? circumstanceModifier : 0}`);
 }
 
-function close() {}
+function close() { }
 
 let errorContent = 'Please select an Actor token and run the macro again';
-let dialogContent = skillsOptions?`
+let dialogContent = skillsOptions ? `
   <script>
     function SkillChanged(selectedSkill) {
       document.getElementById("skillBonus").innerHTML = (selectedSkill.value >= 0? '+': '') + selectedSkill.value;
@@ -63,12 +74,20 @@ let dialogContent = skillsOptions?`
       </th>
       <td style="width:33%">
         <select id="skillDropDown" onchange=SkillChanged(this)>
-          ${skillsOptions.map(e => `<option value="${e.value}" ${e.name == initialSkill ? ' selected' : ''}>${e.name}</option>`).join(``)}
+          <optgroup label="ATTRIBUTES">
+            ${attributes.map(e => `<option value="${e.value}" ${e.name == initialSkill ? ' selected' : ''}>${e.name}</option>`).join(``)}
+          </optgroup>
+          <optgroup label="RESOURCES">
+            ${resources.map(e => `<option value="${e.value}" ${e.name == initialSkill ? ' selected' : ''}>${e.name}</option>`).join(``)}
+          </optgroup>
+          <optgroup label="SKILLS">
+            ${skills.map(e => `<option value="${e.value}" ${e.name == initialSkill ? ' selected' : ''}>${e.name}</option>`).join(``)}
+          </optgroup>
         </select>
       </td>
       <td style="width:33%;border 1px solid black;padding:5px" id="skillBonus">${(initialSkillBonus >= 0 ? '+' : '') + initialSkillBonus}</td>
     </tr>
-    <tr id="attributeBonusRow" style=${initialSkill.includes(['A'])?"display:none": "display:''"}>
+    <tr id="attributeBonusRow" style=${initialSkill.includes(['A']) ? "display:none" : "display:''"}>
       <th style="width:33%">
         <label id="attributeBonusLabel">Attribute Bonus</label>
       </th> 
@@ -93,8 +112,8 @@ let dialogContent = skillsOptions?`
       </td>
     </tr>
   </table>
-  <div id="buffer" style=${initialSkill.includes(['A'])?"height:40px;display:''": "display:none"}></div>
-  `:'';
+  <div id="buffer" style=${initialSkill.includes(['A']) ? "height:40px;display:''" : "display:none"}></div>
+  `: '';
 
 let d = new Dialog({
   title: 'WtC Skill Roll',
